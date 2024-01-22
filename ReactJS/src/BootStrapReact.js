a4bfcb9d6bbf1f51bdca3098c9e7a0fafbb9124c
Aman Yadav <97920298+Amanblackdevil@users.noreply.github.com>
2024-01-22 13:25:33 +0530

    Add files via upload

---
+++ ReactJS/src/BootStrapReact.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function BootStrapReact() {
+    return(
+        <div>
+            <h1>Bootstrap button</h1>
+            <button onClick={() => alert("hello")}>Click me</button>
+        </div>
+    )
+}
+
+export default BootStrapReact;
